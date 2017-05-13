7b1c359cdf1595191e00d97c4b6ac5297d3bdc27
Huynh Thanh Nhan <htnhan93@gmail.com>
2017-05-13 13:33:26 +0700

    Tạo mới project

---
+++ n2-ltdd-doancuoiky/n2-ltdd-doancuoiky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  n2-ltdd-doancuoiky
+//
+//  Created by Huynh Thanh Nhan on 5/13/17.
+//  Copyright © 2017 Huynh Thanh Nhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
